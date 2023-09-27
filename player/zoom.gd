1213704f3f2ac3dd2bd7efc907d6cc2daf90e7b3
iirlas <bitwisetuna@gmail.com>
2023-09-27 15:21:29 -0500

    Renamed Test Scene

--- player/zoom.gd
+++ player/zoom.gd
@@ -5,12 +5,11 @@ extends Node
 @export var min: float = 1.0
 @export var max: float = 2.0
 @export var steps: int = 10
-@onready var current: int = steps / 2
+@onready var current: int = 0
 
 
 func _input(event):
 	if event is InputEventMouseButton and event.is_pressed():
-		print(camera.zoom)
 		match event.button_index:
 			MOUSE_BUTTON_WHEEL_UP:
 				current = clamp(current + 1, 0, steps)
